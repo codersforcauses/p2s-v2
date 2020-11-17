7a3ed25f0b4a3620a248e7d53375b7767269c547
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2020-11-17 17:26:37 +0800

    updated mailer

--- backend/src/services/authmanagement/notifier.js
+++ backend/src/services/authmanagement/notifier.js
@@ -1,4 +1,6 @@
 /* eslint-disable indent */
+const errors = require('@feathersjs/errors');
+
 module.exports = function(app) {
   function getLink(type, hash) {
     // TODO: fix url
@@ -14,9 +16,16 @@ module.exports = function(app) {
         console.log('Sent email', result);
       })
       .catch(err => {
-        console.log('Error sending email', err);
+        console.err('Error sending email', err);
       });
   }
+
+  const getEmail = (user, subjectData, htmlData) => ({
+    from: 'mailer@p2srugbyworks.com',
+    to: user.email,
+    subject: subjectData,
+    html: htmlData,
+  });
 
   return {
     notifier: function(type, user) {
@@ -24,46 +33,53 @@ module.exports = function(app) {
       let email;
       switch (type) {
         case 'resendVerifySignup': //sending the user the verification email
-          tokenLink = getLink('verify', user.verifyToken);
-          email = {
-            from: process.env.FROM_EMAIL,
-            to: user.email,
-            subject: 'Verify Signup',
-            html: tokenLink,
-          };
+          tokenLink = getLink('register', user.verifyToken);
+          email = getEmail(user, 'Verify Signup', tokenLink);
           return sendEmail(email);
 
         case 'verifySignup': // confirming verification
-          tokenLink = getLink('verify', user.verifyToken);
-          email = {
-            from: process.env.FROM_EMAIL,
-            to: user.email,
-            subject: 'Confirm Signup',
-            html: 'Thanks for verifying your email',
-          };
+          tokenLink = getLink('register', user.verifyToken);
+          email = getEmail(
+            user,
+            'Confirm Signup',
+            'Thanks for verifying your email'
+          );
           return sendEmail(email);
 
         case 'sendResetPwd':
           tokenLink = getLink('reset', user.resetToken);
-          email = {};
+          email = getEmail(user, 'Reset Password', tokenLink);
           return sendEmail(email);
 
         case 'resetPwd':
           tokenLink = getLink('reset', user.resetToken);
-          email = {};
+          email = getEmail(
+            user,
+            'Your password was reset',
+            'Your password was reset'
+          );
           return sendEmail(email);
 
         case 'passwordChange':
-          email = {};
+          tokenLink = getLink('verifyChanges', user.verifyToken);
+          email = getEmail(
+            user,
+            'Your password was changed',
+            'Your password was changed'
+          );
           return sendEmail(email);
 
         case 'identityChange':
           tokenLink = getLink('verifyChanges', user.verifyToken);
-          email = {};
+          email = getEmail(
+            user,
+            'Your account was changed. Please verify the changes',
+            tokenLink
+          );
           return sendEmail(email);
 
         default:
-          break;
+          throw errors.GeneralError('Error sending email');
       }
     },
   };
