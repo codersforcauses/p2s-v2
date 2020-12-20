7361474905b726035dbe79a2a6a641a6dda7fad4
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2020-12-20 11:23:05 +0800

    update admin dashboard

--- frontend/src/components/admin/Dashboard.vue
+++ frontend/src/components/admin/Dashboard.vue
@@ -1,34 +1,53 @@
 <template>
-  <div class="layout-grid">
-    <no-coach />
-    <component v-for="card in cards" :key="card" :is="card" />
-  </div>
+  <v-row>
+    <v-col cols="12" sm="6" md="4" xl="3" v-for="i in columns" :key="i" class="py-0">
+      <v-row>
+        <v-col cols="12" v-for="(card, index) in cards" :key="index" v-show="index % columns === i - 1">
+          <component :is="card" />
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      width: 0,
       cards: [
         'staff-card',
-        'region-card',
-        'program-card',
-        'activity-card',
         'session-card',
+        'program-card',
         'student-card',
         'school-card',
       ],
     };
   },
   components: {
-    'no-coach': () => import('./NoRegionCoachCard'),
     'staff-card': () => import('./StaffCard'),
-    'region-card': () => import('./RegionCard'),
     'program-card': () => import('./ProgramCard'),
-    'activity-card': () => import('./ActivityCard'),
     'session-card': () => import('./SessionCard'),
     'student-card': () => import('./StudentCard'),
     'school-card': () => import('./SchoolCard'),
+  },
+  created() {
+    window.addEventListener('resize', this.handleResize);
+    this.handleResize();
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.handleResize);
+  },
+  computed: {
+    columns() {
+      // eslint-disable-next-line
+      return this.width < 600 ? 1 : this.width < 960 ? 2 : 3;
+    },
+  },
+  methods: {
+    handleResize() {
+      this.width = window.innerWidth;
+    },
   },
 };
 </script>
