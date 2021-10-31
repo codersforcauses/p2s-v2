321d8125d6a995f244f81f597fa296adde2bdb7f
Drewbi <drewalexander986@gmail.com>
2021-10-31 22:35:23 +0800

    add functionality to generic list

--- frontend/src/components/layout/List.vue
+++ frontend/src/components/layout/List.vue
@@ -1,27 +1,36 @@
 <template>
-  <v-list two-line subheader>
-    <v-list-item @click="openCreateDialog">
-      <v-icon color="primary">mdi-plus</v-icon>
-      <v-list-item-subtitle class="ml-3" style="color: #f87f79">Add Session</v-list-item-subtitle>
-      <v-slot />
-    </v-list-item>
-    <v-list-item-group v-model="selected" @change="selectSession" mandatory color="primary">
-      <template v-for="item in items">
-        <v-list-item :key="item._id" @click="$emit('open')">
-          <v-list-item-content>
-            <v-slot />
-          </v-list-item-content>
-        </v-list-item>
-      </template>
-    </v-list-item-group>
-  </v-list>
+  <v-skeleton-loader type="list-item-three-line@10" :loading="loading">
+    <v-list two-line subheader>
+      <v-list-item @click="$emit('openAction')">
+        <v-icon color="primary">mdi-plus</v-icon>
+        <v-list-item-subtitle class="ml-3" style="color: #f87f79">Add {{ name }}</v-list-item-subtitle>
+        <slot name="addDialog" />
+      </v-list-item>
+      <v-list-item-group v-model="selected" @change="$emit('makeSelection', items[selected])" mandatory color="primary">
+        <template v-for="item in items">
+          <v-list-item :key="item._id" @click="$emit('openDrawer')">
+            <slot name="itemDisplay" :item="item" />
+          </v-list-item>
+        </template>
+      </v-list-item-group>
+    </v-list>
+  </v-skeleton-loader>
 </template>
 
 <script>
 
 export default {
   props: {
-    items: Array
+    name: String,
+    items: Array,
+    loading: Boolean
   }
 }
 </script>
+
+// Usage:
+// <UserList name="Staff" :items="users">
+//   <template v-slot:itemDisplay="props">
+//     <UserListDisplay :user="props.item" />
+//   </template>
+// </UserList>
