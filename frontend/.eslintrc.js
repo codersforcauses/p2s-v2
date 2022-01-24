6670a735921fe13a99d6c9589d1a0edda45cf476
Drewbi <drewalexander986@gmail.com>
2022-01-24 11:11:44 +0800

    fix babel errors

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: ['plugin:vue/essential', 'airbnb-base', 'prettier'],
   parserOptions: {
     parser: '@babel/eslint-parser',
+    "requireConfigFile": false
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
