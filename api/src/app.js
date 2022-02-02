305b782457e0c04bdad4e1243c677a2cf49d705e
Drewbi <drewalexander986@gmail.com>
2022-02-02 12:47:14 +0800

    update env vars

--- api/src/app.js
+++ api/src/app.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const path = require('path');
 const favicon = require('serve-favicon');
