8a19a464266c16c31c6211e4754ab9d0c28cfae4
Drewbi <drewalexander986@gmail.com>
2022-02-08 00:21:26 +0800

    add component name rule

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-underscore-dangle': 'off',
+    'vue/multi-word-component-names': 'off',
     'import/extensions': 0,
   },
 };
