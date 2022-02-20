fbaf11a3de2b28888bec84cce38ae587ae66b0b4
Drewbi <drewalexander986@gmail.com>
2022-02-20 16:43:21 +0800

    fix location display

--- app/src/components/Session/ActivityView.vue
+++ app/src/components/Session/ActivityView.vue
@@ -6,7 +6,7 @@
   >
     <div class="d-flex flex-column">
       <div class="text-body-1 text-sm-h6 text-center text-md-h5 primary--text">{{ school ? school.name : 'loading' }}</div>
-      <div class="text-center">{{ session.type }}</div>
+      <div class="text-center">{{ session.type }} {{ session.location ? ` - ${session.location}` : ''}}</div>
     </div>
   </FeathersVuexGet>
 </template>
