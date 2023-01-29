6448521dbcbac94e8431fe99272312e1bed817ec
Drew Alexander <drewalexander986@gmail.com>
2023-01-29 11:44:17 +0800

    Improve startup logging

--- api/src/mongoose.js
+++ api/src/mongoose.js
@@ -2,14 +2,15 @@ const mongoose = require('mongoose');
 const logger = require('./logger');
 
 module.exports = function (app) {
-  mongoose
+    logger.info('Attempting to connect to database...')
+    mongoose
     .connect(app.get('mongodb'))
-    .then((info) => logger.info('Database connection established: ' + info.connections[0].name))
+    .then((info) => logger.info(`Database connection established: ${info.connections[0].name}`))
     .catch((err) => {
-      throw(new Error(err));
+      throw new Error(`Failed to establish database connection: ${err}`);
     });
-
-  mongoose.Promise = global.Promise;
-
-  app.set('mongooseClient', mongoose);
+    
+    mongoose.Promise = global.Promise;
+    
+    app.set('mongooseClient', mongoose);
 };
