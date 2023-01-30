baac13987b029df6af8bdb70ac4d6b63d65122de
Drew Alexander <drewalexander986@gmail.com>
2023-01-30 22:24:00 +0800

    add updates to dashboard cards

--- app/src/components/Session/SessionCard/index.vue
+++ app/src/components/Session/SessionCard/index.vue
@@ -1,47 +1,43 @@
 <template>
-  <v-card flat rounded="xl">
-      <v-card flat class="rounded-tl-xl" style="overflow: hidden;">
+  <Card title="Sessions">
+    <template v-slot:content>
+      <v-card flat>
         <v-tabs v-model="selectedTab" class="mb-2">
           <v-tab class="text-capitalize text-body-2">Next Session</v-tab>
           <v-tab class="text-capitalize text-body-2">Last Session</v-tab>
         </v-tabs>
-        <v-container v-if="session">
-          <v-row class="mx-3">
+        <div v-if="session">
+          <div class="d-flex pb-2">
             <DateView :date="session.date" />
-            <v-divider
-            inset vertical
-            class="mx-5"
-            />
-            <FeathersVuexGet 
-              v-slot="{ item: school }"
-              service="schools"
-              :id="session.school"  
-            >
+            <v-divider inset vertical class="mx-5" />
+            <FeathersVuexGet v-slot="{ item: school }" service="schools" :id="session.school">
               <div class="d-flex flex-column justify-space-between">
-                <div class="text-subtitle-1">{{ formatTime(session) }} - {{ school ? school.name : 'loading' }}</div>
+                <div class="text-subtitle-1">{{ formatTime(session) }} - {{ school? school.name : 'loading' }}</div>
                 <div>{{ session.type }}</div>
                 <div>{{ coachText(session) }}</div>
               </div>
             </FeathersVuexGet>
-          </v-row>
-        </v-container>
-        <div v-else class="text-h6 mb-10 px-4">
-          No Session Found
+          </div>
+          <v-btn width="100%" outlined rounded color="primary" :to="{ path: `/session/${session._id}` }">
+            View Session
+          </v-btn>
         </div>
-        <v-btn v-if="session" text rounded color="primary" :to="{ path: `/session/${session._id}`}">View Session</v-btn>
+        <div v-else class="text-body">
+          No {{ selectedTab === 0 ? "upcoming" : "previous" }} sessions found
+        </div>
       </v-card>
-    <v-card-title primary-title class="primary--text text-h6 pb-0">Manage Sessions</v-card-title>
-    <v-card-text>
-      View all the sessions on file
-      <br /><span v-if="adminUser">Create a new session to be used in lessons or programs</span>
-    </v-card-text>
-
-    <v-card-actions class="py-1">
+    </template>
+    <template v-slot:actions>
       <v-btn text rounded color="primary" :to="{ name: 'view-sessions' }">View All</v-btn>
-      <v-btn v-if="adminUser" text rounded color="primary" @click="sessionDialog = true">Create New</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn v-if="adminUser" text rounded color="primary" @click="sessionDialog = true">
+        <v-icon left>
+          mdi-plus
+        </v-icon>
+        Create New</v-btn>
       <SessionDialog v-if="adminUser" v-model="sessionDialog" />
-    </v-card-actions>
-  </v-card>
+    </template>
+  </Card>
 </template>
 
 <script>
@@ -50,6 +46,7 @@ import { makeFindMixin } from 'feathers-vuex'
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
 
+import Card from '../../layout/Card.vue'
 import SessionDialog from '../SessionDialog'
 import DateView from '../DateView.vue'
 import userRoleMixin from '../../../utils/userRole.mixin';
@@ -57,8 +54,9 @@ import userRoleMixin from '../../../utils/userRole.mixin';
 dayjs.extend(utc)
 
 export default {
-  mixins: [ makeFindMixin({ service: 'sessions', watch: true }), userRoleMixin],
+  mixins: [makeFindMixin({ service: 'sessions', watch: true }), userRoleMixin],
   components: {
+    Card,
     SessionDialog,
     DateView
   },
@@ -81,22 +79,22 @@ export default {
     searchQuery() {
       const currentDate = dayjs().utc().format('YYYY-MM-DDTHH:mm:ss[Z]')
       const query = this.selectedTab === 0 ?
-      {
-        date: {
-          $gte: currentDate
-        },
-        $sort: {
-          date: 1
+        {
+          date: {
+            $gte: currentDate
+          },
+          $sort: {
+            date: 1
+          }
+        } :
+        {
+          date: {
+            $lt: currentDate
+          },
+          $sort: {
+            date: -1
+          }
         }
-      } :
-       {
-        date: {
-          $lt: currentDate
-        },
-        $sort: {
-          date: -1
-        }
-      }
       return query
     },
   },
@@ -112,7 +110,7 @@ export default {
 </script>
 
 <style scoped>
-  .subbutton {
-    text-transform: none;
-  }
+.subbutton {
+  text-transform: none;
+}
 </style>
