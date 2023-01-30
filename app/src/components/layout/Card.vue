12b92f1beb6383f060504cf2b492e9dd49ad1bba
Drew Alexander <drewalexander986@gmail.com>
2023-01-30 22:22:58 +0800

    Add generalised card

---
+++ app/src/components/layout/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card flat rounded="xl" class="d-flex flex-column justify-space-between">
+    <v-card-title primary-title class="text-h4 pb-0">{{ title }}</v-card-title>
+    <v-card-text>
+      <slot name="content"></slot>
+    </v-card-text>
+    <v-card-actions>
+      <slot name="actions"></slot>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+
+export default {
+  props: {
+    title: String
+  }
+}
+</script>
