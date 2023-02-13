898075f369b503d3fb825f0eb68ca438d5f2af33
Drew Alexander <drewalexander986@gmail.com>
2023-02-13 21:41:58 +0800

    remove random oauth strategy

--- api/src/services/auth/auth.service.js
+++ api/src/services/auth/auth.service.js
@@ -1,6 +1,6 @@
 const { AuthenticationService, JWTStrategy } = require('@feathersjs/authentication');
 const { LocalStrategy } = require('@feathersjs/authentication-local');
-const { expressOauth } = require('@feathersjs/authentication-oauth');
+const authHooks = require('./auth.hooks')
 
 module.exports = app => {
   const authentication = new AuthenticationService(app);
@@ -9,5 +9,6 @@ module.exports = app => {
   authentication.register('local', new LocalStrategy());
 
   app.use('/authentication', authentication);
-  app.configure(expressOauth());
+  const authService = app.service('authentication')
+  authService.hooks(authHooks)
 };
